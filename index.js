1212bae488af6bafae3d60f6c6f628d1517f98ec
Dr. Shashikant <shashimech092@gmail.com>
2023-06-20 17:12:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+<script>
+        function myfunction(){
+         let x=document.getElementById('demo').value
+         const show = document.getElementById('display')
+         try{
+             if(x.trim()=='') throw 'empty'
+             if(isNaN(x)) throw 'not a number'
+             x=Number(x);
+             if(x<5) throw 'too low'
+             if(x>10) throw 'too high'
+         }
+         catch(err){
+             show.innerHTML='Input is '+ err
+         }
+         finally {
+             document.getElementById("demo").value = "";
+        }
+        } 
+     </script>
